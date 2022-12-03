3f791be0737837cd288c2575ff53941b9744ec00
Dreslei <dresleigb@gmail.com>
2022-12-02 22:40:00 -0300

    Primeira versão

---
+++ app/utils/view.php
@@ -0,0 +1,22 @@
+<?php 
+
+    namespace App\utils;    
+
+    class view{
+
+        private static function getContentView($view) {
+            $file = __DIR__ .'/../../resources/view/' .$view.'.html';
+            return file_exists($file) ? file_get_contents($file) : 'Não encontrou o conteudo';
+        }
+
+        public static function render($view, $vars = []) {
+            $contentView = self::getContentView($view);
+            $keys = array_keys($vars);
+            $keys = array_map(function($item){
+                return '{{'.$item.'}}';
+            },$keys);
+
+            return str_replace($keys,array_values($vars),$contentView);
+        }
+    }
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+    require __DIR__.'/vendor/autoload.php';
+
+    use \App\controller\pages\home;
+
+    echo home::gethome();
+?>
